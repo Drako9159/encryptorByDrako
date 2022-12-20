f373313219e6c835fb6c1f098df548602ee15a5f
drako9159 <drako9159@gmail.com>
2022-12-20 00:10:43 -0600

    add animation

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const btnEncrypt = document.querySelector("#encrypt");
 const btnDecrypt = document.querySelector("#decrypt");
 const displayEncrypt = document.querySelector("#displayEncrypt");
 const copyText = document.querySelector("#copyText");
+const animationLock = document.querySelector(".animation");
+const haveAnimation = document.querySelector("#have-animation");
 
 function encrypt(letter) {
   const values = {
@@ -31,22 +33,32 @@ function decrypt(letter) {
   });
   return check;
 }
+function activeAnimation(lock) {
+  let pushed = `<img src="./icons/${lock}.svg" class="animation" alt="lock-unlock">`;
+  haveAnimation.innerHTML = pushed;
+}
+
 btnEncrypt.addEventListener("click", (e) => {
+  if (writter.value) activeAnimation("lock");
   let check = "";
   for (let letter in writter.value) {
     check += encrypt(writter.value[letter]);
   }
   displayEncrypt.value = check;
+  writter.value = "";
 });
 
 btnDecrypt.addEventListener("click", (e) => {
+  if (writter.value) activeAnimation("unlock");
+
   displayEncrypt.value = decrypt(writter.value);
+  writter.value = "";
 });
 
 copyText.addEventListener("click", async (e) => {
   displayEncrypt.focus();
   displayEncrypt.select();
-  
+
   await navigator.clipboard.writeText(displayEncrypt.value);
   /*
   try {
