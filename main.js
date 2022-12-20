c3f2a74852b6728e129f9f4966a603fa2afd198c
drako9159 <drako9159@gmail.com>
2022-12-19 20:01:32 -0600

    add complete functionality

---
+++ main.js
@@ -0,0 +1,50 @@
+const writter = document.querySelector("#writter");
+const btnEncrypt = document.querySelector("#encrypt");
+const btnDecrypt = document.querySelector("#decrypt");
+const displayEncrypt = document.querySelector("#displayEncrypt");
+const copyText = document.querySelector("#copyText");
+
+function encrypt(letter) {
+  const values = {
+    e: "enter",
+    i: "imes",
+    a: "ai",
+    o: "ober",
+    u: "ufat",
+  };
+  if (values[letter]) return values[letter];
+  return letter;
+}
+function decrypt(letter) {
+  let check = "";
+  const exp = [
+    { e: /enter/g },
+    { i: /imes/g },
+    { a: /ai/g },
+    { o: /ober/g },
+    { u: /ufat/g },
+  ];
+
+  exp.forEach((e) => {
+    letter = letter.replace(Object.values(e)[0], Object.keys(e)[0]);
+    check = letter;
+  });
+  return check;
+}
+btnEncrypt.addEventListener("click", (e) => {
+  let check = "";
+  for (let letter in writter.value) {
+    check += encrypt(writter.value[letter]);
+  }
+  displayEncrypt.value = check;
+});
+
+btnDecrypt.addEventListener("click", (e) => {
+  displayEncrypt.value = decrypt(writter.value);
+});
+
+copyText.addEventListener("click", async (e) => {
+  displayEncrypt.select();
+  displayEncrypt.setSelectionRange(0, 99999);
+  await navigator.clipboard.writeText(displayEncrypt.value);
+});
